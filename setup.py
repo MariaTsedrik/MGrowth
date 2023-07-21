f24e6dbefc91de3206a011acca67ae1a25f15b82
Maria Tsedrik <tsedrikmaria@google.com>
2023-07-21 19:17:57 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+#def get_requires():
+#    reqs = []
+#    for line in open("requirements.txt", "r").readlines():
+#        reqs.append(line)
+#    return reqs
+
+setup(
+    name="MGrowth",
+    version=0.0,
+    url="https://github.com/MariaTsedrik/MGrowth",
+    author="",
+    author_email="",
+    license="BSD",
+    packages=find_packages(),
+    keywords="Modified Gravity Extended Cosmology",
+#    install_requires=get_requires(),
+)
